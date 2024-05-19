ce35879825c0eeee5e696dd84624d31e0f40767b
Aritra Sarkar <aritrasarkar2002@gmail.com>
2024-05-19 20:47:55 +0530

    .

--- frontend/src/components/UserButton.jsx
+++ frontend/src/components/UserButton.jsx
@@ -1,44 +1,83 @@
-import { LogOut } from "lucide-react"
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Clipboard, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useEffect, useState } from "react";
 import useWallet from "@/context/UseWallet";
 import { ipfsDownload } from "@/utils/ipfs";
 import { patientInfoCid } from "@/utils/patient";
 import { doctorInfoCid } from "@/utils/doctor";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import toast from "react-hot-toast";
 
 export const UserButton = () => {
-    const [data, setData] = useState(null);
-    const {role, address, contract } = useWallet();
-    useEffect(() => {
-        (async function () {
-          let infoCid;
-          if (role === 1) infoCid = await patientInfoCid({ address, contract });
-          if (role === 2) infoCid = await doctorInfoCid({ address, contract }); 
-          if (infoCid) {
-            const data = await ipfsDownload(infoCid);
-            setData(data);
-          }
-        })();
-      }, [role, address, contract]);
-      console.log(data);
-      if(!data){
-        return null;
+  const [data, setData] = useState(null);
+  const { role, address, contract } = useWallet();
+
+  useEffect(() => {
+    (async function () {
+      let infoCid;
+      if (role === 1) infoCid = await patientInfoCid({ address, contract });
+      if (role === 2) infoCid = await doctorInfoCid({ address, contract });
+      if (infoCid) {
+        const data = await ipfsDownload(infoCid);
+        setData(data);
       }
+    })();
+  }, [role, address, contract]);
+
+  const abbreviatedAddress = `${address.slice(0, 8)}...${address.slice(-6)}`;
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(address).then(
+      () => {
+        toast.success("Address copied to clipboard!");
+      },
+      (err) => {
+        toast.error("Could not copy text: ", err);
+      }
+    );
+  };
+
+  if (!data) {
+    return null;
+  }
+
   return (
     <div className="container shadow-sm border-l-4 border-transparent flex justify-between w-full">
-        <div className='flex items-center gap-4 h-full'>
-            <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <div className='flex flex-col h-full justify-between flex-1'>
-                <p className='text-sm text-white font-light'>{data.name}</p>
-                <p className="text-sm text-zinc-400 line-clamp-1 font-extralight hover:cursor-pointer">{address.substr(0,15)}...</p>
-            </div>
+      <div className="flex items-center gap-4 h-full">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col h-full justify-between flex-1">
+          <p className="text-sm text-white font-light">{data.name}</p>
+          <TooltipProvider>
+            <Tooltip>
+              <div className="flex items-center gap-4">
+                <TooltipTrigger className="text-sm text-zinc-400 line-clamp-1 font-extralight hover:cursor-pointer">
+                  {abbreviatedAddress}
+                </TooltipTrigger>
+                <Clipboard
+                  size={15}
+                  strokeWidth={1}
+                  className="text-white cursor-pointer"
+                  onClick={copyToClipboard}
+                />
+              </div>
+              <TooltipContent className="text-white bg-neutral-800">
+                {address}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
-        <button className="text-white bg-transparent hover:text-zinc-500 transition">
-            <LogOut size={"19"} />
-        </button>
+      </div>
+      <button className="text-white bg-transparent hover:text-zinc-500 transition">
+        <LogOut size={"19"} />
+      </button>
     </div>
-  )
-}
+  );
+};
--- frontend/src/components/container/Sidebar.jsx
+++ frontend/src/components/container/Sidebar.jsx
@@ -1,52 +1,116 @@
-import React from 'react'
-import useWallet from '@/context/UseWallet';
-import { Link, useResolvedPath } from 'react-router-dom';
-import { Album, LayoutGrid, LibraryBig, LogOut, Newspaper, UserRound, Zap } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { Button } from '../ui/button';
-import { UserButton } from '../UserButton';
+import React from "react";
+import useWallet from "@/context/UseWallet";
+import { Link, useResolvedPath } from "react-router-dom";
+import {
+  Album,
+  LayoutGrid,
+  LibraryBig,
+  Newspaper,
+  UserRound,
+  Zap,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import { UserButton } from "../UserButton";
+
+const navLinks = {
+  0: [
+    {
+      to: "/register",
+      label: "Register",
+      icon: <UserRound size={20} strokeWidth={2} />,
+    },
+  ],
+  1: [
+    {
+      to: "/dashboard",
+      label: "Dashboard",
+      icon: <LayoutGrid size={20} strokeWidth={2} />,
+    },
+    {
+      to: "/patient/bookAppointment",
+      label: "Book appointment",
+      icon: <Album size={20} strokeWidth={2} />,
+    },
+    {
+      to: "/patient/allAppointments",
+      label: "All Appointments",
+      icon: <Newspaper size={20} strokeWidth={2} />,
+    },
+    {
+      to: "/patient/allRecords",
+      label: "All Records",
+      icon: <LibraryBig size={20} strokeWidth={2} />,
+    },
+    {
+      to: "/patient/allEmergencyRecords",
+      label: "All Emergency Records",
+      icon: <Zap size={20} strokeWidth={2} />,
+    },
+  ],
+  2: [
+    {
+      to: "/dashboard",
+      label: "Dashboard",
+      icon: <LayoutGrid size={20} strokeWidth={2} />,
+    },
+    {
+      to: "/doctor/viewAppointments",
+      label: "View Appointments",
+      icon: <Newspaper size={20} strokeWidth={2} />,
+    },
+  ],
+};
 
 export const Sidebar = () => {
   const { role } = useWallet();
-  const pathname = useResolvedPath().pathname
-  
-   const navLinks = {
-    0: [{ to: "/register", label: "Register", icon: <UserRound size={20} strokeWidth={2} />}],
-    1: [
-      { to: "/dashboard", label: "Dashboard", icon: <LayoutGrid size={20} strokeWidth={2} /> },
-      { to: "/patient/bookAppointment", label: "Book appointment", icon: <Album size={20} strokeWidth={2} />},
-      { to: "/patient/allAppointments", label: "All Appointments", icon: <Newspaper size={20} strokeWidth={2} />},
-      { to: "/patient/allRecords", label: "All Records", icon: <LibraryBig size={20} strokeWidth={2} />},
-      { to: "/patient/allEmergencyRecords", label: "All Emergency Records", icon: <Zap size={20} strokeWidth={2} />},
-    ],
-    2: [
-      { to: "/dashboard", label: "Dashboard", icon: <LayoutGrid size={20} strokeWidth={2} />},
-      { to: "/doctor/viewAppointments", label: "View Appointments", icon: <Newspaper size={20} strokeWidth={2} />},
-    ],
-  };
+  const pathname = useResolvedPath().pathname;
+
+  const { getSigner } = useWallet();
+
+  async function handleConnect() {
+    await getSigner();
+  }
+
   return (
-    <aside className="flex flex-col justify-between bg-[#333333] w-1/5 py-10">
-        <div className='flex flex-col gap-10 items-center text-white'>
-            <Link className='text-xl font-bold' to="dashboard">
-                Mediblock
-            </Link>
-            <ul className='flex flex-col gap-3 w-full'>
-                {navLinks[role].map((nav, i) => {
-                    return  (
-                        <li key={i} className={cn('text-zinc-400 border-l-4 border-transparent font-light', pathname === nav.to && "text-white border-l-4 border-blue-300 font-medium")}>
-                            <Link to={nav.to} className='flex items-center gap-5 text-sm pl-12 py-3'>
-                                {nav.icon}
-                                {nav.label}
-                            </Link>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-        {role !== 0 && (     
-            <UserButton />
-        )}
+    <aside className="flex flex-col justify-between bg-neutral-800 w-1/5 py-10">
+      <div className="flex flex-col gap-10 items-center text-white">
+        <Link className="text-xl font-bold" to="dashboard">
+          Mediblock
+        </Link>
+        <ul className="flex flex-col gap-3 w-full">
+          {navLinks[role].map((nav, i) => {
+            return (
+              <li
+                key={i}
+                className={cn(
+                  "text-zinc-400 border-l-4 border-transparent font-light",
+                  pathname === nav.to &&
+                    "text-white border-l-4 border-blue-300 font-medium"
+                )}
+              >
+                <Link
+                  to={nav.to}
+                  className="flex items-center gap-5 text-sm pl-12 py-3"
+                >
+                  {nav.icon}
+                  {nav.label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {role !== 0 ? (
+        <UserButton />
+      ) : (
+        <Button
+          onClick={handleConnect}
+          className="py-6 w-11/12 mx-auto rounded-xl flex items-center text-lg"
+        >
+          Connect
+        </Button>
+      )}
     </aside>
-  )
-}
+  );
+};
